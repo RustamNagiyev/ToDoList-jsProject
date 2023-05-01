28b2c6eda6ac9b9dd22637a1a1a2de194d6f245e
RustamNagiyev <755.optiplex@mail.ru>
2023-05-02 00:42:18 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,58 @@
+const input = document.getElementById("input");
+const addBtn = document.getElementById("addBtn");
+const clearBtn = document.getElementById("clearBtn");
+const sortBtn = document.getElementById("sortBtn");
+const list = document.getElementById("list");
+const sortImg = document.getElementById("sortImg");
+let flag = true;
+let ascendingOrder = true;
+
+sortBtn.addEventListener("click", () => {
+     if (flag) {
+        sortImg.src = "./imaj/Group91.svg";
+        console.log(sortImg.src)
+        flag = !flag
+      } else {
+        sortImg.src = "./imaj/Group73.svg";
+        console.log(sortImg.src)
+        flag = !flag
+      } 
+  const items = Array.from(list.children).sort((a, b) => {
+    const aValue = a.firstElementChild.textContent.toLowerCase();
+    const bValue = b.firstElementChild.textContent.toLowerCase();
+    
+    if (aValue < bValue) {
+      return ascendingOrder ? -1 : 1;
+    } else if (aValue > bValue) {
+      return ascendingOrder ? 1 : -1;
+    } else {
+      return 0;
+    }
+});
+
+  list.innerHTML = "";
+  items.forEach((item) => {
+    list.appendChild(item);
+});
+  ascendingOrder = !ascendingOrder;
+});
+
+clearBtn.addEventListener("click", () => {
+  input.value = "";
+});
+addBtn.addEventListener("click", () => {
+  if (input.value !== "") {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    span.textContent = input.value;
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "x";
+    deleteBtn.addEventListener("click", () => {
+      list.removeChild(li);
+    });
+    li.appendChild(span);
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
+    input.value = "";
+}
+});
